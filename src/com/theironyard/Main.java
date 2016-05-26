4cb81d93fba2d3d0b53b62ce339265cf640d429a
halfshelldell <simmonslydell@gmai.com>
2016-05-26 11:43:45 -0400

    add refactor

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -7,11 +7,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static ArrayList<Post> parsePosts(String filename) throws FileNotFoundException {
         ArrayList<Post> posts = new ArrayList<>();
 
-        // Parse File
-        File f = new File("posts.txt");
+        File f = new File(filename);
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
@@ -20,19 +19,31 @@ public class Main {
             Post post = new Post(Integer.valueOf(columns[0]), columns[1], columns[2]);
             posts.add(post);
         }
+        return posts;
+    }
+
+    public static void printPost(ArrayList<Post> posts, int currentPost) {
+        int postId = 0;
+        for (Post post : posts) {
+            if (post.replyId == currentPost) {
+                System.out.printf("[%s] %s by %s\n", postId, post.text, post.author);
+            }
+            postId++;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Parse File
+        ArrayList<Post> posts = parsePosts("posts.txt");
 
         // start loop
         Scanner consoleScanner = new Scanner(System.in);
         int currentPost = -1;
         while (true) {
             // print out the replies to currentPost
-            int postId = 0;
-            for (Post post : posts) {
-                if (post.replyId == currentPost) {
-                    System.out.printf("[%s] %s by %s\n", postId, post.text, post.author);
-                }
-                postId++;
-            }
+            printPost(posts, currentPost);
+
             // ask for new id
             System.out.println("Type the id you want to see replies to:");
             currentPost = Integer.valueOf(consoleScanner.nextLine());
